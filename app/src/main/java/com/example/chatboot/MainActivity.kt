d61fc2909c231b4abfb8b10372e61dac1a7ab4e8
sonuhalder24 <haldersonu36@gmail.com>
2021-08-05 02:22:59 +0530

    ui modification

--- app/src/main/java/com/example/chatboot/MainActivity.kt
+++ app/src/main/java/com/example/chatboot/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.chatboot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var count=1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hideTxt.setOnClickListener({
+            if(count==1){
+                count=0
+                hideTxt.text=getString(R.string.sign_up_txt)
+                signUpBtn.text="Log In"
+                forgot.visibility= View.GONE
+                filledConfirmPassword.visibility=View.GONE
+            }
+            else{
+                count=1
+                hideTxt.text=getString(R.string.log_in_txt)
+                signUpBtn.text="Sign Up"
+                forgot.visibility= View.VISIBLE
+                filledConfirmPassword.visibility=View.VISIBLE
+            }
+        })
     }
 }
