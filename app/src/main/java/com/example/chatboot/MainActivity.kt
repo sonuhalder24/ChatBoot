acfc156c51cb7eb83442a34f9403845dad1ceafa
sonuhalder24 <haldersonu36@gmail.com>
2021-08-06 18:22:35 +0530

    login backend added

--- app/src/main/java/com/example/chatboot/MainActivity.kt
+++ app/src/main/java/com/example/chatboot/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chatboot
 
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+
     var count=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
+        progress.visibility=View.GONE
         hideTxt.text=getString(R.string.sign_up_txt)
         signUp.text="Log In"
         forgot.visibility= View.VISIBLE
@@ -39,61 +42,111 @@ class MainActivity : AppCompatActivity() {
                 forgot.visibility= View.VISIBLE
                 filledConfirmPassword.visibility=View.GONE
                 filledName.visibility=View.GONE
+                editEmail.setText("")
+                editPassword.setText("")
+                filledPassword.setHelperText("Required*")
             }
-            else{
+            else if(count==0){
                 count=1
                 hideTxt.text=getString(R.string.log_in_txt)
                 signUp.text="Sign Up"
                 forgot.visibility= View.GONE
                 filledConfirmPassword.visibility=View.VISIBLE
                 filledName.visibility=View.VISIBLE
+                editName.setText("")
+                editEmail.setText("")
+                editPassword.setText("")
+                editConfirmPassword.setText("")
+                filledPassword.setHelperText("Required*")
             }
         })
 
         signUp.setOnClickListener({
-            if(editName.text.toString().trim().isEmpty()|| editEmail.text.toString().trim().isEmpty()
-                || editPassword.text.toString().trim().isEmpty() || editConfirmPassword.text.toString().trim().isEmpty())
-            {
-                Toast.makeText(this,"Please fill all credentials",Toast.LENGTH_SHORT).show()
+            if(count==1) {
+                if (editName.text.toString().trim().isEmpty() || editEmail.text.toString().trim().isEmpty()
+                || editPassword.text.toString().trim().isEmpty() || editConfirmPassword.text.toString().trim().isEmpty()) {
+                    Toast.makeText(this, "Please fill all credentials", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    if (editPassword.text.toString().trim().length < 6) {
+                        filledPassword.setHelperText("Week Password")
+
+                    } else if (editPassword.text.toString().trim().length >= 6) {
+                        if (editPassword.text.toString().trim()
+                                .contains('@') || editPassword.text.toString().trim()
+                                .contains('&') ||
+                            editPassword.text.toString().trim().contains('%')
+                        ) {
+                            filledPassword.setHelperText("Strong Password")
+
+                            if (!editPassword.text.toString().trim()
+                                    .equals(editConfirmPassword.text.toString().trim())
+                            ) {
+                                Toast.makeText(this, "Passwords are different", Toast.LENGTH_SHORT)
+                                    .show()
+                            } else {
+                                progress.visibility=View.VISIBLE
+                                val email: String = editEmail.text.toString().trim { it <= ' ' }
+                                val pass: String = editPassword.text.toString().trim { it <= ' ' }
+
+                                auth.createUserWithEmailAndPassword(email, pass)
+                                    .addOnCompleteListener(this) { task ->
+                                        if (task.isSuccessful) {
+                                            Toast.makeText(this, "You registered successfully", Toast.LENGTH_SHORT).show()
+                                            val user = auth.currentUser
+                                            updateUI(user)
+                                        } else {
+                                            Toast.makeText(this, "Sign up failed", Toast.LENGTH_SHORT).show()
+                                            updateUI(null)
+                                        }
+                                    }
+                            }
+                        } else {
+                            filledPassword.setHelperText("Not so strong Password")
+                        }
+                    }
+
+                }
             }
-            else{
-                if(editPassword.text.toString().trim().length<6){
-                    filledPassword.setHelperText("Week Password")
-
+            else if(count==0){
+                if (editEmail.text.toString().trim().isEmpty() || editPassword.text.toString().trim().isEmpty()) {
+                    Toast.makeText(this, "Please fill all credentials", Toast.LENGTH_SHORT).show()
                 }
-                else if(editPassword.text.toString().trim().length>=6){
-                   if(editPassword.text.toString().trim().contains('@')||editPassword.text.toString().trim().contains('&')||
-                       editPassword.text.toString().trim().contains('%')){
-                       filledPassword.setHelperText("Strong Password")
-
-                       if(!editPassword.text.toString().trim().equals(editConfirmPassword.text.toString().trim())){
-                           Toast.makeText(this,"Passwords are different",Toast.LENGTH_SHORT).show()
-                       }
-                       else{
-                           val email: String=editEmail.text.toString().trim{it <=' '}
-                           val pass: String=editPassword.text.toString().trim{it <= ' '}
-
-                           auth.createUserWithEmailAndPassword(email,pass)
-                               .addOnCompleteListener(this) {    task ->
-                                   if(task.isSuccessful){
-//                               val user:FirebaseUser?= auth.currentUser
-                                       Toast.makeText(this,"You registered successfully",Toast.LENGTH_SHORT).show()
-                                   }
-                                   else{
-                                       Toast.makeText(this,"Something wrong",Toast.LENGTH_SHORT).show()
-                                   }
-                               }
-                       }
-                   }
-                    else{
-                       filledPassword.setHelperText("Not so strong Password")
-                   }
+                else{
+                    progress.visibility=View.VISIBLE
+                    val email: String = editEmail.text.toString().trim { it <= ' ' }
+                    val pass: String = editPassword.text.toString().trim { it <= ' ' }
+                        auth.signInWithEmailAndPassword(email,pass)
+                            .addOnCompleteListener(this){   task->
+                                if(task.isSuccessful){
+                                    Toast.makeText(this, "Successfully logged in", Toast.LENGTH_SHORT).show()
+                                    val user = auth.currentUser
+                                    updateUI(user)
+                                }
+                                else{
+                                    Toast.makeText(this,"Login failed",Toast.LENGTH_SHORT).show()
+                                    updateUI(null)
+                                }
+                            }
                 }
-
             }
         })
     }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
 
+    private fun updateUI(user: FirebaseUser?) {
+        if(user==null){
+            progress.visibility=View.GONE
+        }
+        else{
+            startActivity(Intent(this,ChatActivity::class.java))
+            finish()
+        }
+    }
 }
 
 
